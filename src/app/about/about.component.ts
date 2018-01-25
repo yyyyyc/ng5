0448e8fcc575f90e5cdb485224bada3b08078663
Yiftach Cohen <Yiftachc@KCSYS.local>
2018-01-25 18:35:43 +0200

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+//import { ActivatedRoute } from '@angular/router/src/router_state';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) { 
+
+      this.route.params.subscribe (res => console.log (res.id));
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate (['']);
+  }
+
+}
